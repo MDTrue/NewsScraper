8468b4d3711c77323e3e8ab85614df78d81af8ce
MDTrue <MDTrueII@hotmail.com>
2019-01-23 09:44:59 -0800

    updated model link

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var path = require("path")
 var axios = require("axios");
 var cheerio = require("cheerio");
 // Require all models
-var db = require("./models");
+var db = require("/models");
 
 var PORT = process.env.PORT || 3000;
 
